2f791626252ff6044f397bf9056741e40c1f4a0b
brian <brianjoseph.egan@gmail.com>
2023-12-24 18:07:19 -0800

    Add Code and Test Project
    Add projects with skeleton implementation for LeetCode problem.

---
+++ LeetCode/LeetCode.Tests/AddTwoNumbers/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.AddTwoNumbers;
+
+namespace LeetCode.Tests.AddTwoNumbers
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Example1()
+        {
+            var listNode1 = new ListNode(3);
+            listNode1 = new ListNode(4, listNode1);
+            listNode1 = new ListNode(2, listNode1);
+
+            var listNode2 = new ListNode(4);
+            listNode2 = new ListNode(6, listNode2);
+            listNode2 = new ListNode(5, listNode2);
+
+            var solution = new Solution();
+            var listNode = solution.AddTwoNumbers(listNode1, listNode2);
+
+            listNode.Should().NotBeNull();
+        }
+    }
+}
---
+++ LeetCode/LeetCode/AddTwoNumbers/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.AddTwoNumbers
+{
+    internal class Solution
+    {
+        internal ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
